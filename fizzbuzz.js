30566fc289dc2dc9eac7d7c72802d3bddcc77b2c
Enrico Digman <essdigman@gmail.com>
2023-01-24 07:21:46 +0800

    Added test for not a positive integer

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,12 @@
 export function fizzBuzz(number){
   let resultString = ''
 
-  if (number % 3 === 0) resultString += 'fizz'
-  if (number % 5 === 0) resultString += 'buzz'
+  if (number >= 0) {
+    if (number % 3 === 0) resultString += 'fizz'
+    if (number % 5 === 0) resultString += 'buzz'
+  } else {
+    throw new Error('Accepts only postive integer')
+  }
 
   return resultString
 }
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -2,39 +2,28 @@ import { expect, test } from '@jest/globals'
 import { fizzBuzz } from './fizzbuzz.js'
 
 describe('FizzBuzz Tests', () => {
-  test('returns " " for number 2', () => {
-    expect(fizzBuzz(1)).toEqual('')
+  test('returns error for not a positive integer', () => {
+    expect(() => fizzBuzz(-5)).toThrow('Accepts only postive integer')
   })
 
-  test('returns " " for number 8', () => {
+  test('returns " " if number not divisible by 3 and 5', () => {
+    expect(fizzBuzz(1)).toEqual('')
     expect(fizzBuzz(8)).toEqual('')
   })
 
-  test('returns "fizz" for number 24', () => {
-    expect(fizzBuzz(24)).toEqual('fizz')
+  test('returns "fizzbuzz" if number divisible by 3 and 5', () => {
+    expect(fizzBuzz(0)).toEqual('fizzbuzz')
+    expect(fizzBuzz(30)).toEqual('fizzbuzz')
+    expect(fizzBuzz(90)).toEqual('fizzbuzz')
   })
 
-  test('returns "fizz" for number 33', () => {
+  test('returns "fizz" if number divisible by 3', () => {
+    expect(fizzBuzz(24)).toEqual('fizz')
     expect(fizzBuzz(33)).toEqual('fizz')
   })
 
-  test('returns "buzz" for number 20', () => {
+  test('returns "buzz" if number divisible by 5', () => {
     expect(fizzBuzz(20)).toEqual('buzz')
-  })
-
-  test('returns "buzz" for number 50', () => {
     expect(fizzBuzz(50)).toEqual('buzz')
-  })
-
-  test('returns "fizzbuzz" for number 0', () => {
-    expect(fizzBuzz(0)).toEqual('fizzbuzz')
-    })
-
-  test('returns "fizzbuzz" for number 30', () => {
-    expect(fizzBuzz(30)).toEqual('fizzbuzz')
-  })
-
-  test('returns "fizzbuzz" for the number 90', () => {
-    expect(fizzBuzz(90)).toEqual('fizzbuzz')
   })
 })
