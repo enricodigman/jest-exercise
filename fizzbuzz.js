64a4cc93099a79f5854d3d820bc0b538f7860763
Enrico Digman <essdigman@gmail.com>
2023-01-23 14:18:38 +0800

    Revised fizzBuzz function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,8 @@
 export function fizzBuzz(number){
   let resultString = ''
 
-  if (number % 3 === 0 && number % 5 === 0) {
-    resultString += 'fizzbuzz'
-  } else if (number % 3 === 0) {
-    resultString += 'fizz'
-  } else if (number % 5 === 0) {
-    resultString += 'buzz'
-  } else {
-    resultString += ''
-  }
+  if (number % 3 === 0) resultString += 'fizz'
+  if (number % 5 === 0) resultString += 'buzz'
 
   return resultString
 }
