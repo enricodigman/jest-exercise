86ee41bc9ae0bc5409b307cb3020dd50aa4f13ca
Enrico Digman <essdigman@gmail.com>
2023-01-24 09:19:41 +0800

    Fixed function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,16 @@
 export default function fizzBuzz(number){
   let resultString = ''
 
-  if (Number.isInteger(number) && number >= 0) {
-    if (number % 3 === 0) {
-      resultString += 'fizz'
-    } 
-    if (number % 5 === 0) {
-      resultString += 'buzz'
-    } 
-  } else {
+  if (!Number.isInteger(number) || number < 0) {
     throw new Error('Accepts only postive integer')
   }
+
+  if (number % 3 === 0) {
+    resultString += 'fizz'
+  } 
+  if (number % 5 === 0) {
+    resultString += 'buzz'
+  } 
 
   return resultString
 }
